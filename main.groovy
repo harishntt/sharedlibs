121991ffc94c6cd190f9974bcb84045c93be3df4
harishntt <35359360+harishntt@users.noreply.github.com>
2022-06-28 20:17:28 +0530

    Update main.groovy

--- main.groovy
+++ main.groovy
@@ -2,6 +2,23 @@
 
 pipeline{
     agent any
+        properties([
+        parameters([
+            githubProjectProperty(displayName: 'repo', 
+                                projectUrlStr: 'https://github.com/harishntt/sharedlibs.git')
+            gitParameter(branch: '', 
+                        branchFilter: '.*', 
+                        defaultValue: 'dev', 
+                        description: 'Select the branch', 
+                        name: 'Branch', 
+                        quickFilterEnabled: false, 
+                        selectedValue: 'NONE', 
+                        sortMode: 'NONE', 
+                        tagFilter: '*', 
+                        type: 'GitParameterDefinition')
+        ])
+    ])
+
     stages{
         stage('Test'){
             steps{
